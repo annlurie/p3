ee2a9fd853098957b404d8d3bf2073868383bdf3
AnnLurie <lurie@g.harvard.edu>
2014-10-23 11:11:37 -0400

    Formatting changes - results now appear to the right of input form.

--- app/views/loremipsum.blade.php
+++ app/views/loremipsum.blade.php
@@ -6,68 +6,70 @@ Fake Text Maker
 
 @section ('content')
 
-<!--PHP Logic Section-->
-
+<!--Header 'Jumbotron' with intro text-->
     <div class="jumbotron">
       <div class="container">
         <h1>Fake Text Maker</h1>
         <p>Here's the text that describes how to fake some text.</p>
       </div>
     </div>
+<!--End Header-->
 
-<form class="form-horizontal">
-<fieldset>
-
-<!-- Text input-->
+<!--Two-column body: Left column is input form, Right is output (if there's anything to output)-->
 <div class="container">
+  <div class="row">
+  	<!--Left column with input form-->
+    <div class="col-md-4">
+		<form class="form-horizontal" action = "/lipsum">
+		<fieldset>
+			<!-- Text input-->
+			<div class="container">
 
-<div class="control-group">
-  <label class="control-label" for="number">How Many Paragraphs?</label>
-  <div class="controls">
-    <input id="number" name="number" placeholder="1-99" class="input-mini" type="text">
-    
-  </div>
-</div>
+			<div class="control-group">
+			  <label class="control-label" for="number">How Many Paragraphs?</label>
+			  <div class="controls">
+			    <input id="number" name="number" placeholder="1-99" class="input-mini" type="text">		    
+			  </div>
+			</div>
+			<!-- Button -->
+			<div class="control-group">
+			  <label class="control-label" for="submit"></label>
+			  <div class="controls">
+			    <button id="submit" name="submit" class="btn btn-primary">Fake It Up!</button>
+			  </div>
+			</div>
+			</div>
+		</fieldset>
+		</form>
+    </div>
+    <!--End left column-->
 
-<!-- Button -->
-<div class="control-group">
-  <label class="control-label" for="submit"></label>
-  <div class="controls">
-    <button id="submit" name="submit" class="btn btn-primary">Fake It Up!</button>
-  </div>
-</div>
-
-</div> <!--close container-->
-
-<!--Place for output-->
-<div class="container">
-<?php
-	if (isset($data))
-	{
-		//echo Pre::render($data);
-		if ((int)$data < 0)
-		{
-			$count = 1;
-		}
-		if ((int)$data > 99)
-		{
-			$count = 99;
-		}
-
-		else $count = $data;
-		$generator = new LoremIpsum();
-	    $paragraphs = $generator->getParagraphs($count);
-	}
-?>
+    <div class="col-md-8">
+		<?php
+			if (isset($data))
+			{
+				//echo Pre::render($data);
+				if ((int)$data < 0)
+				{
+					$count = 1;
+				}
+				if ((int)$data > 99)
+				{
+					$count = 99;
+				}
+				else $count = $data;
+				$generator = new LoremIpsum();
+			    $paragraphs = $generator->getParagraphs($count);
+			}
+		?>
 
 		@if (isset($data))
 			<h2>Here is your text:</h2>
 		   	<p>{{implode('<p>', $paragraphs);}}</p>
 	   	@endif
+	   	</div>
 
+  <hr>
 </div>
-
-</fieldset>
-</form>
 
 @stop
